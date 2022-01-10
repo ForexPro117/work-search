88442d9ae6b2f9437bc1da52be1b4d3a3c5efc26
ForexPro <roma.minihanowa@mail.ru>
2022-01-10 15:51:21 +0400

    забыл добавить удаление файла

--- app/Http/Controllers/ResponseController.php
+++ app/Http/Controllers/ResponseController.php
@@ -11,8 +11,9 @@ class ResponseController extends Controller
     public function create(Request $request)
     {
 
-        return view('response',['workId'=>$request->workId]);
+        return view('response', ['workId' => $request->workId]);
     }
+
     public function make(Request $request)
     {
         $request->validate([
@@ -20,49 +21,52 @@ class ResponseController extends Controller
             'file' => 'required|file'
         ]);
 
-        $response=new Response();
-        $response->description=$request->description;
-        $response->user_id=$request->user()->id;
-        $response->work_id=$request->workId;
-        $response->file=Storage::put('public', $request->file('file'));
-        $response->file_original_name=$request->file('file')->getClientOriginalName();
+        $response = new Response();
+        $response->description = $request->description;
+        $response->user_id = $request->user()->id;
+        $response->work_id = $request->workId;
+        $response->file = Storage::put('public', $request->file('file'));
+        $response->file_original_name = $request->file('file')->getClientOriginalName();
         $response->save();
-  return view('banner',['text'=>'Вы успешно оставили свой отклик,']);
+        return view('banner', ['text' => 'Вы успешно оставили свой отклик,']);
     }
-
 
 
     public function checkUserHistory(Request $request)
     {
 
-        $responses=Response::where('response.user_id',$request->user()->id)
+        $responses = Response::where('response.user_id', $request->user()->id)
             ->leftJoin('works', 'response.work_id', '=', 'works.id')
             ->leftJoin('employer_data', 'works.employer_id', '=', 'employer_data.employer_id')
-            ->select('response.description as des','response.file',
-                'response.file_original_name','works.*','employer_data.*')
+            ->select('response.description as des', 'response.file',
+                'response.file_original_name', 'works.*', 'employer_data.*')
             ->get();
-        return view('historyUser',['responses'=>$responses]);
+        return view('historyUser', ['responses' => $responses]);
     }
+
     public function checkHistory(Request $request)
     {
 
-        $responses=Response::leftJoin('works', 'response.work_id', '=', 'works.id')
+        $responses = Response::leftJoin('works', 'response.work_id', '=', 'works.id')
             ->leftJoin('user_data', 'response.user_id', '=', 'user_data.user_id')
-            ->where('works.employer_id',$request->user()->id)
-            ->select('response.id as responseId','response.description as des','response.file',
-                'response.file_original_name','works.*','user_data.*')
+            ->where('works.employer_id', $request->user()->id)
+            ->select('response.id as responseId', 'response.description as des', 'response.file',
+                'response.file_original_name', 'works.*', 'user_data.*')
             ->get();
-        return view('history',['responses'=>$responses]);
+        return view('history', ['responses' => $responses]);
     }
+
     public function download(Request $request)
     {
 
         return Storage::download($request->name, $request->origname);
     }
+
     public function delete(Request $request)
     {
-     Response::find($request->responseId)->delete();
-
+        $response = Response::find($request->responseId);
+        Storage::delete($response->file);
+        $response->delete();
         return 'good';
     }
 }
